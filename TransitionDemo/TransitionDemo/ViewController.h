6f227894bd87bf419218d4f04d32703efad14849
Yioks-Mac <392811726@qq.com>
2017-02-24 16:49:26 +0800

    init
    init

---
+++ TransitionDemo/TransitionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionDemo
+//
+//  Created by Yioks-Mac on 17/2/24.
+//  Copyright © 2017年 Yioks-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TransitionDemo/TransitionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TransitionDemo
+//
+//  Created by Yioks-Mac on 17/2/24.
+//  Copyright © 2017年 Yioks-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
